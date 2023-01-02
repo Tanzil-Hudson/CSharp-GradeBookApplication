87c8d0190b1f57811b7541aec4b3477c900deeb6
Tanzil Hudson <tanzil.hudson@gmail.com>
2023-01-01 22:53:02 -0500

    update 2
    Checking work

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Linq;
-
-using GradeBook.Enums;
-using System.Collections.Generic;
-using System.IO;
+﻿using GradeBook.Enums;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
-        public GradeBookType { get; set; }
+
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {
@@ -264,7 +265,7 @@ namespace GradeBook.GradeBooks
                              from type in assembly.GetTypes()
                              where type.FullName == "GradeBook.GradeBooks.StandardGradeBook"
                              select type).FirstOrDefault();
-            
+
             return JsonConvert.DeserializeObject(json, gradebook);
         }
     }
