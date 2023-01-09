f6fc6996ab24cb6be912ca55f41096aeac8b3a90
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-01-09 13:40:22 -0500

    Revert "Update RankedGradeBook.cs"
    This reverts commit 17fee89e4454ddea7388bdb37551e9ca4779488d.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,9 +1,6 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Net.Security;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -15,26 +12,5 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
-        public override char GetLetterGrade(double averageGrade)
-        {
-            if(Students.Count < 5)
-                throw new InvalidOperationException("Ranked Grading required 5 or more students");
-
-            var threshould = (int)Math.Ceiling(Student.Count * .2);
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-
-            if(grades[threshould - 1] <= averageGrade)
-                return 'A';
-            else if (grades[(threshould*2) - 1] <= averageGrade)
-                return 'B';
-            else if (grades[(threshould*3) - 1] <= averageGrade)
-                return 'C';
-            else if (grades[(threshould*4) - 1] <= averageGrade)
-                return 'D';
-            else
-                return 'F';
-
-
-        }
     }
 }
