0718697929711a1adffb059cd360667cce7e75a6
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-01-09 14:52:27 -0500

    Jan09.05

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,7 +18,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked grading requires at least 5 students");
 
-            var threshold = (int)Math.Ceiling(Students.Count * 2);
+            var threshold = (int)Math.Ceiling(Students.Count * .2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (grades[threshold - 1] <= averageGrade)
