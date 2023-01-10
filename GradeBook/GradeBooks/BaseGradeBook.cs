ce058662f7ed147140da1fb261369f883825aa9f
Tanzil-Hudson <113275165+Tanzil-Hudson@users.noreply.github.com>
2023-01-09 23:01:27 -0500

    Final

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -22,6 +22,7 @@ namespace GradeBook.GradeBooks
             Name = name;
             Students = new List<Student>();
             Type = new GradeBookType();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -110,20 +111,29 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                gpa++;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
